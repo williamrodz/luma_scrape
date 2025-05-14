1a175687433110eba69bf2ea9f4c915d0052e115
William A Rodriguez <william.a@alum.mit.edu>
2025-05-13 23:37:48 -0400

    Added playwright install

--- scrape_service_status.py
+++ scrape_service_status.py
@@ -17,8 +17,6 @@ import os
 # Supabase credentials from environment
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
-print(f"URL: {SUPABASE_URL[0:5]}")  # Print only part of the key for security
-print(f"KEY: {SUPABASE_KEY[0:5]}")  # Print only part of the key for security
 
 def save_data_to_supabase(data):
     """
