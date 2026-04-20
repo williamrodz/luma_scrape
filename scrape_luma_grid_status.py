0eae7f7a2c02ab7feca5de21a67499e984e9a851
William A Rodriguez <william.a@alum.mit.edu>
2026-04-20 16:45:52 -0400

    added headers para coolear against 403

--- scrape_luma_grid_status.py
+++ scrape_luma_grid_status.py
@@ -23,8 +23,13 @@ URL = "https://lumapr.com/system-overview/?lang=en"
 DEFAULT_HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/123.0 Safari/537.36"
-    )
+        "(KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
 }
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
